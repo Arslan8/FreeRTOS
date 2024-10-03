bcb578836e3a688779c65625a1ba4d651cc041ef
Arslan8 <arslankhan52@gmail.com>
2024-10-02 20:44:13 -0400

    Comitting working demo for STM32f769i-EVAL board

---
+++ FreeRTOS/Demo/STM32F769i-EVAL/setup.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/ARM-software/CMSIS_5.git
+cd CMSIS-5/Device
+git clone https://github.com/STMicroelectronics/cmsis_device_f7
+cd -
+perl -pe 'BEGIN{undef $/;} s|/\*.*?\*/||gs; s|//.*||g' CMSIS-5//Device/cmsis_device_f7/Source/Templates/gcc/startup_stm32f769xx.s > startup.s
