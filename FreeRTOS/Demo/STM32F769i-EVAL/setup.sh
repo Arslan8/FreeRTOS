829ca7f63c3d6137e1afd2250fcdb11db1057f85
Arslan8 <arslankhan52@gmail.com>
2024-10-02 20:49:20 -0400

    Stupid path

--- FreeRTOS/Demo/STM32F769i-EVAL/setup.sh
+++ FreeRTOS/Demo/STM32F769i-EVAL/setup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/ARM-software/CMSIS_5.git
-cd CMSIS-5/Device
+cd CMSIS_5/Device
 git clone https://github.com/STMicroelectronics/cmsis_device_f7
 cd -
-perl -pe 'BEGIN{undef $/;} s|/\*.*?\*/||gs; s|//.*||g' CMSIS-5//Device/cmsis_device_f7/Source/Templates/gcc/startup_stm32f769xx.s > startup.s
+perl -pe 'BEGIN{undef $/;} s|/\*.*?\*/||gs; s|//.*||g' CMSIS_5//Device/cmsis_device_f7/Source/Templates/gcc/startup_stm32f769xx.s > startup.s
